d11745cdcf552260188008eceb0466ab013d0b6b
YogeshNegi10 <yogeshlm13@gmail.com>
2025-04-29 14:51:55 +0530

    first commit

--- Frontend/utils/api.js
+++ Frontend/utils/api.js
@@ -1,5 +1,5 @@
-export const server = "http://localhost:5000"
+export const server = "https://tasktracker-ratz.onrender.com"
 
 
-
+// https://tasktracker-ratz.onrender.com
 // http://localhost:5000
