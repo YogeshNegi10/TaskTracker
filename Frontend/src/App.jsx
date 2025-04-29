9ae79e8ae4663c98f95b78e69c87396b966cb7db
YogeshNegi10 <yogeshlm13@gmail.com>
2025-04-29 14:54:02 +0530

    bug fix

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -68,9 +68,9 @@ function App() {
           <Route
             path="/"
             element={
-              <ProtectedRoute>
+              
                 <Home />
-              </ProtectedRoute>
+             
             }
           />
           <Route
